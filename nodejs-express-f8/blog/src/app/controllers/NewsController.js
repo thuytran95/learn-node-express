41a006b8f63da9ed5e2e66516dc9a3b3384ff70c
Tran Thuy <thuytt0295@gmail.com>
2021-06-10 23:55:22 +0700

    add routes, controller in MVC model

---
+++ nodejs-express-f8/blog/src/app/controllers/NewsController.js
@@ -0,0 +1,14 @@
+class NewsController {
+	// tuyen duong luon khop tu tren xuong -> phai de tuyen duong trong o cuoi
+	//  [GET] /news/:slug
+	show(req, res) {
+		res.send('new details');
+	}
+
+	// [GET] /news
+	index(req, res) {
+		res.render('news');
+	}
+}
+
+module.exports = new NewsController();
--- nodejs-express-f8/blog/src/index.js
+++ nodejs-express-f8/blog/src/index.js
@@ -5,27 +5,27 @@ const handelebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 // static file & SCSS
 app.use(express.static(path.join(__dirname, 'public')));
 
+// middleware xu ly body: formData: urlencoded, fetch,axios, XMLRequest-> json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // HTTP logger - middleware
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handelebars({ extname: '.hbs' })); //define
 app.set('view engine', 'hbs'); //set view engine-> name of handlebars
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-	res.render('home');
-});
-
-app.get('/news', (req, res) => {
-	res.render('news');
-});
+// Routes init
+route(app);
 
 // local host IP: 127.0.0.1 -> localhost
-
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
